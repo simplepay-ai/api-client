161fd1e525dbcccd1060198970acf6413facde14
Catzilla <11861208+Catzilla@users.noreply.github.com>
2024-08-24 00:43:08 +0300

    Small fix

--- src/BaseChannel.ts
+++ src/BaseChannel.ts
@@ -1,5 +1,12 @@
+import camelcaseKeys from 'camelcase-keys';
 import { Channel } from 'pusher-js';
 
 export default class BaseChannel {
     constructor(protected channel: Channel) {}
+
+    protected toCamelCase(obj: any): object {
+        return camelcaseKeys(obj, {
+            deep: true
+        });
+    }
 }
--- src/channels/InvoiceChannel.ts
+++ src/channels/InvoiceChannel.ts
@@ -4,6 +4,6 @@ import { InvoiceStatus } from '../models';
 
 export default class InvoiceChannel extends BaseChannel {
     public on(status: InvoiceStatus, callback: (invoice: Invoice) => void): void {
-        this.channel.bind(status, callback);
+        this.channel.bind(status, (i: object) => callback(this.toCamelCase(i) as Invoice));
     }
 }
