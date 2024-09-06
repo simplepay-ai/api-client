566598a4cd7e98cc6889ffffaecc1c3c583b1642
Catzilla <11861208+Catzilla@users.noreply.github.com>
2024-09-06 23:03:38 +0300

    Export channels

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './errors';
 export * from './models';
 export * from './requests';
 export * from './services';
+export * from './channels';
 export * from './Client';
 export * from './WsClient';
