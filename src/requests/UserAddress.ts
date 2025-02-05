5c47982b86fddb780d6e299a3fe7ff3501e2d958
Catzilla <11861208+Catzilla@users.noreply.github.com>
2025-02-05 13:45:19 +0200

    Small fix

--- src/requests/UserAddress.ts
+++ src/requests/UserAddress.ts
@@ -2,13 +2,6 @@ import { UserAddressType } from '../models';
 
 export type UserAddressCreateRequest = {
     /**
-     * Address ID
-     *
-     * @example '0660bfde-e576-467a-af63-69edaf85f8c2'
-     */
-    id: string;
-
-    /**
      * Address name
      *
      * @example 'Main Ethereum wallet'
